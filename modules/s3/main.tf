a6c24d26671e6ca62d3ce68cf050e31836fe8239
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-09 22:13:41 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -2,7 +2,7 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
   force_destroy = var.bucket_force_destroy
-  tags              = var.tags
+  #tags              = var.tags
 }
 
 
