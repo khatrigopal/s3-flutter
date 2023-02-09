308d54412386b68b8703427fed76ca2ffae758a3
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-09 21:30:01 +0530

    Update s3.tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,6 +1,6 @@
 ######### S3##############
 module "s3" {
-  source     = "git::https://github.com/Flutter-Global/dtpl-terraform-modules.git//modules/s3"
+  source     = "../modules/s3"
   bucket_name  = "dtpl-insights-${local.environment-khatrig}-s3"
   #tags              = var.tags
   versioning = var.versioning
