750b524d68b16399bdc52fb4cdf7614293a33034
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-09 23:14:14 +0530

    Update s3.tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -5,7 +5,7 @@ locals {
 ######### S3###############
 module "s3" {
   source     = "../modules/s3"
-  bucket_name  = "dtpl-insigte-khatrig-${local.testing}-s3"
+  bucket_name  = "dtpl-insigte-dev-khatrig-s3"
   tags              = var.tags
   versioning = var.versioning
   acl        = var.s3_acl
