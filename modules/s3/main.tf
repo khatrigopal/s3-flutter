96803cee854d95153b6785607f74e3628c8d48fb
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-09 21:10:23 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -24,3 +24,4 @@ resource "aws_s3_bucket_public_access_block" "public_access_block" {
   ignore_public_acls      = var.s3_ignore_public_acls
   restrict_public_buckets = var.s3_restrict_public_buckets
 }
+
