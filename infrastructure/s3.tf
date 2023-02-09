6118a58d40ffad2b34f2435c6e4395858d8b0002
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-09 22:11:44 +0530

    Update s3.tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -1,7 +1,7 @@
 ######### S3###############
 module "s3" {
   source     = "../modules/s3"
-  bucket_name  = "dtpl-insights-khatrig-s3"
+  bucket_name  = "var.bucket_name"
   tags              = var.tags
   versioning = var.versioning
   acl        = var.s3_acl
