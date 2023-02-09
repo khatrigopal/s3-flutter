0888d62e573abfd6ea08d2ef2c61d438397556dd
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-09 15:26:41 +0000

    Create main.tf

---
+++ modules/s3/main.tf
@@ -0,0 +1,26 @@
+#S3 Bucket on Which we will add policy
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = var.bucket_name
+  force_destroy = var.bucket_force_destroy
+  tags              = var.tags
+}
+
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.test-bucket.id
+  versioning_configuration {
+    status = var.versioning
+  }
+}
+resource "aws_s3_bucket_acl" "acl" {
+  bucket = aws_s3_bucket.test-bucket.id
+  acl    = var.acl
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket                  = aws_s3_bucket.s3_bucket.id
+  block_public_acls       = var.s3_block_public_acls
+  block_public_policy     = var.s3_block_public_policy
+  ignore_public_acls      = var.s3_ignore_public_acls
+  restrict_public_buckets = var.s3_restrict_public_buckets
+}
