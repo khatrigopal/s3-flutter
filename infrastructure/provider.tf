e20b30075716b50759315aebffd8b9d988b52142
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-09 15:31:49 +0000

    Create provider.tf

---
+++ infrastructure/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">=1.1.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+  default_tags {
+    tags = {
+      platform    = var.platform
+      project     = var.project
+      environment = local.environment
+      cost_centre  = var.cost_centre
+      code_repo    = var.code_repo
+    }
+  }
