3d2b23b46c0c5a692dabed95ac797141a5c2e305
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-02-09 23:16:56 +0530

    Update s3.tf

--- infrastructure/s3.tf
+++ infrastructure/s3.tf
@@ -6,7 +6,7 @@ locals {
 module "s3" {
   source     = "../modules/s3"
   bucket_name  = var.bucket_name
-  tags              = var.tags
+  #tags              = var.tags
   versioning = var.versioning
   acl        = var.s3_acl
 }
